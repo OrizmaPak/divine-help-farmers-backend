8a379291b89a88564740d41d9bc931f88e8cacbb
oreva jovisam orior <orevaorior@gmail.com>
2025-02-15 11:39:37 +0100

    update

--- routes/offline.js
+++ routes/offline.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getofflineready } = require('../controllers/offline/getready');
+const { offlineProcessCollection } = require('../controllers/offline/submitpayments');
 const router = express.Router();
 
 
@@ -7,6 +8,9 @@ const router = express.Router();
 // CREATE INVENTORY
 router.route('/getready')
     .get(getofflineready)
+
+router.route('/submitpayments')
+    .post(offlineProcessCollection)
 
 
     
