38c09507cb9ac803b0d564b4273bdc71456a2f20
oreva jovisam orior <orevaorior@gmail.com>
2025-06-20 21:53:54 +0100

    Update transaction reference handling in saveTransaction function
    - Modified the `saveTransaction` function in the transaction helper utility to use nullish coalescing for the `transactionref` field, ensuring that it defaults to an empty string if not provided. This change enhances the robustness of transaction data processing and maintains consistency in handling transaction references.

--- utils/transactionHelper.js
+++ utils/transactionHelper.js
@@ -283,7 +283,7 @@ const saveTransaction = async (client, res, transactionData, req) => {
 
         await client.query(
             `INSERT INTO divine."transaction" (accountnumber, credit, debit, reference, description, ttype, status, transactiondate, whichaccount, valuedate, transactiondesc, dateadded, createdby, currency, userid, tfrom, transactionref, cashref, branch) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, now(), $12, $13, $14, $15, $16, $17, $18)`,
-            [accountnumber, credit, debit, newReference, description, ttype, status, transactiondate, req.body.whichaccount, finalValuedate, transactiondesc, createdBy, currency, userid, tfrom, transactionref, cashref, req.body.branch]
+            [accountnumber, credit, debit, newReference, description, ttype, status, transactiondate, req.body.whichaccount, finalValuedate, transactiondesc, createdBy, currency, userid, tfrom, transactionref??req.body.transactionref??'', cashref, req.body.branch]
         );
 
         // Extract the phone number from the personal account number
